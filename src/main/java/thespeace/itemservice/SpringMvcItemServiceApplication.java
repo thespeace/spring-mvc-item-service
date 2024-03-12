c47ff2a55dbd4e04c0ac5546c3bcdf95e9c6696a
thespeace <thespeace1@gmail.com>
2024-03-12 16:51:24 +0900

    Initial commit

---
+++ src/main/java/thespeace/itemservice/SpringMvcItemServiceApplication.java
@@ -0,0 +1,13 @@
+package thespeace.itemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcItemServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/thespeace/itemservice/SpringMvcItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package thespeace.itemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcItemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
